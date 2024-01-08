51e49e1025ab8e83ae10ac128ac61ea02ea22ae5
musaifk <130353598+musaifk@users.noreply.github.com>
2024-01-08 15:43:13 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "ajay-mrcloudbook777" # Replace with your actual S3 bucket name
+    bucket = "musaifdevops"
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    region = "ap-southeast-1"
   }
 }
